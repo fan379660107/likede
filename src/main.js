adfbeabce72bb8d69941545672042288561ccffc
fan379660107 <379660107@qq.com>
2022-07-30 21:26:08 +0800

    登录页面样式

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
-
+import '@/assets/fonts/iconfont.css'
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
